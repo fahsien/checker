ebaba23f6eb02fea765aebab871a3cf946357642
陳法憲 <tp6z83vu04@chenfaxiandeMacBook-Pro.local>
2016-10-08 17:46:51 +0800

    modify static path

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-var server = app;
+var http = require('http'),
+	express = require('express'),
+	app = express(),
+	path = require('path'),
+	server = app;
 
 //set port
 var port = process.env.PORT || 8080; 
 
 //static files' location
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.resolve('./public')));
 
 app.get('/',function(req, res){ 
 	res.sendfile('./public/views/index.html');
