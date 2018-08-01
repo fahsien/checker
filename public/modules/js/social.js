b247dbbe4a3890d9866a5f44bf223a5a8d3552ad
具有舞台魅力的工程師法憲 <fahsien@bitahdeMac-mini-2.local>
2018-08-01 23:41:34 +0800

    Add social page

--- public/modules/js/social.js
+++ public/modules/js/social.js
@@ -1,3 +1,25 @@
 'use strict';
 
-angular.module('checker').controller('socialController', ['$scope', function ($scope) {}]);
+angular.module('checker').controller('socialController', ['$scope', '$sce', function ($scope, $sce) {
+    setTimeout(function () {
+
+        $scope.$apply(function () {
+            $scope.fbArray = window.fbArray;
+        });
+        console.log($scope.fbArray);
+    }, 2000);
+    // $scope.fb_iframe = function(url){
+    //     return $sce.trustAsResourceUrl(url);
+    // };
+
+    $scope.convertTime = function (UNIX_timestamp) {
+        var a = new Date(UNIX_timestamp * 1000);
+        var year = a.getFullYear();
+        var month = a.getMonth();
+        var date = a.getDate();
+        var hour = '0' + a.getHours();
+        var min = '0' + a.getMinutes();
+        var time = year + '年' + month + '月' + date + '日' + ' ' + hour.substr(-2) + ':' + min.substr(-2);
+        return time;
+    };
+}]);
